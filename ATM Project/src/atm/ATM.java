c8870f13b8f7fde9de6e22067f6c0213ce9fea0a
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2019-03-05 21:30:01 +0100

    ATM fix

---
+++ ATM Project/src/atm/ATM.java
@@ -0,0 +1,19 @@
+package atm;
+
+import java.io.IOException;
+
+public class ATM extends OptionMenu {
+
+	public static void main(String[] args) {
+
+		OptionMenu optionMenu = new OptionMenu();
+		
+	  
+			try {
+				optionMenu.getLogin();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+	}
+
+}
